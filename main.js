2fb842c9d6b4a4c8b11305130aa0aeb2e79ea0dc
Singee <git@singee.me>
2020-09-05 21:53:33 +0800

    support custom component

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
-import { createElement, render } from "./toy-react";
+import { createElement, render, Component } from "./toy-react";
+
+class MyComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h1>My Component!</h1>
+                { this.children }
+            </div>
+        );
+    }
+}
 
 render(
-    <div id="a" class="b" xx="c">
-        <div></div>
-        <div></div>
-        <div></div>
-        777
-    </div>
+    <MyComponent>
+        <div id="a" class="b" xx="c">
+            <div>23</div>
+            <div>66</div>
+            <div>999</div>
+            777
+        </div>
+    </MyComponent>
 , document.body);
--- toy-react.js
+++ toy-react.js
@@ -1,21 +1,77 @@
-export function createElement(elementName, attributes, ...children) {
-    const e = document.createElement(elementName);
+
+class ElementWrapper {
+    constructor(elementName) {
+        this.root = document.createElement(elementName);
+    }
+
+    setAttribute(key, value) {
+        this.root.setAttribute(key, value);
+    }
+
+    appendChild(childComponent) {
+        this.root.appendChild(childComponent.root);
+    }
+}
+
+class TextWrapper {
+    constructor(content) {
+        this.root = document.createTextNode(content);
+    }
+}
+
+export class Component {
+    constructor() {
+        this.props = Object.create(null);
+        this.children = [];
+        this._root = null;
+    }
+
+    setAttribute(key, value) {
+        this.props[key] = value;
+    }
+
+    appendChild(childComponent) {
+        this.children.push(childComponent);
+    }
+
+    get root() {
+        if (!this._root) {
+            this._root = this.render().root;
+        }
+
+        return this._root;
+    }
+}
+
+export function createElement(component, attributes, ...children) {
+    let e;
+    if (typeof component === "string") { // 原生组件
+        e = new ElementWrapper(component);
+    } else { // 自定义组件
+        e = new component;
+    }
 
     for (let k in attributes) {
         e.setAttribute(k, attributes[k]);
     }
 
-    for (let child of children) {
-        if (typeof child === "string") {
-            e.appendChild(document.createTextNode(child));
-        } else {
-            e.appendChild(child);
-        }   
+    const appendChildren = children => {
+        for (let child of children) {
+            if (typeof child === "string") {
+                e.appendChild(new TextWrapper(child));
+            } else if (typeof child === "object" && child instanceof Array) {
+                appendChildren(child);
+            } else {
+                e.appendChild(child);
+            }   
+        }
     }
+
+    appendChildren(children);
 
     return e;
 }
 
 export function render(component, parentElement) {
-    parentElement.appendChild(component);
+    parentElement.appendChild(component.root);
 }
