a3beda49d3afa783dc02ac9cf9772ff90519aa88
Singee <git@singee.me>
2020-09-05 21:24:20 +0800

    implement createElement (for real DOM)

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-const e = <div>123</div>;
+function createElement(elementName, attributes, ...children) {
+    const e = document.createElement(elementName);
+
+    for (let k in attributes) {
+        e.setAttribute(k, attributes[k]);
+    }
+
+    for (let child of children) {
+        if (typeof child === "string") {
+            e.appendChild(document.createTextNode(child));
+        } else {
+            e.appendChild(child);
+        }   
+    }
+
+    return e;
+}
+
+
+
+document.body.appendChild (
+    <div id="a" class="b" xx="c">
+        <div></div>
+        <div></div>
+        <div></div>
+        777
+    </div>
+);
